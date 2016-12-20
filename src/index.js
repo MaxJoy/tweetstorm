83007f2db2f72f272ef2cc142c02b70ce33a86ce
David Wilson <w.dave.w@gmail.com>
2016-12-20 00:56:20 -0800

    add timeline

--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Feed from './components/Feed';
-import { StreamProvider, Timeline, createSuperstream, createStore } from 'omnistream';
+import { StreamProvider, Timeline, createOmnistream, createStore } from 'omnistream';
 import { input$, tweet$ } from './actions/actionStreams'
 import formReducer from './reducers/formReducer'
 import tweetsReducer from './reducers/tweetsReducer'
 
 
-const superstream = createSuperstream();
-const inputState$ = superstream.createStatestream(formReducer, input$);
-const tweetState$ = superstream.createStatestream(tweetsReducer, tweet$);
+const omnistream = createOmnistream();
+const inputState$ = omnistream.createStatestream(formReducer, input$);
+const tweetState$ = omnistream.createStatestream(tweetsReducer, tweet$);
 const streamCollection = { inputState$, tweetState$ };
 
-superstream.createStore(streamCollection);
+omnistream.createStore(streamCollection);
 
-ReactDOM.render((
-  <StreamProvider superstream={superstream}>
+ReactDOM.render(
+  <StreamProvider omnistream={omnistream}>
     <App />
-  </StreamProvider>
-), document.getElementById('root'))
+</StreamProvider>, document.getElementById('root'))
+
+
+   // <Timeline omnistream={omnistream} />
